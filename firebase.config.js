de24ef75fede10dc6b62bcfbbf48142b143d0b9f
RidoyMia <ridoymia153159hero@gmail.com>
2023-11-10 08:25:24 -0800

    firebase config updated

--- firebase.config.js
+++ firebase.config.js
@@ -1,23 +1,26 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+let firebaseConfig;
+if (typeof window !== 'undefined') {
+  firebaseConfig = {
+    apiKey: "AIzaSyDCtkT9ESTPoUPfhFNDfGbPFLz7D07CD8A",
+    authDomain: "red-onion-3e36c.firebaseapp.com",
+    projectId: "red-onion-3e36c",
+    storageBucket: "red-onion-3e36c.appspot.com",
+    messagingSenderId: "904821850512",
+    appId: "1:904821850512:web:f1d5d5422ba0a7d7f3b315",
+    measurementId: "G-8QXPBYDX8H"
+  };
+}
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDCtkT9ESTPoUPfhFNDfGbPFLz7D07CD8A",
-  authDomain: "red-onion-3e36c.firebaseapp.com",
-  projectId: "red-onion-3e36c",
-  storageBucket: "red-onion-3e36c.appspot.com",
-  messagingSenderId: "904821850512",
-  appId: "1:904821850512:web:f1d5d5422ba0a7d7f3b315",
-  measurementId: "G-8QXPBYDX8H"
-};
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 
 // Initialize Firebase
-let app;
-if (typeof window !== 'undefined') {
-  app = initializeApp(firebaseConfig);
-}
+const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-
-export default app;
+export { app, analytics };
--- src/app/(withLoyout)/page.js
+++ src/app/(withLoyout)/page.js
@@ -2,12 +2,17 @@
 
 
 
-import React from 'react';
+import React, { useEffect } from 'react';
+import {analytics} from "../../../firebase.config"
 import Blog from '../../components/Blog/Blog';
 import Services from '../../components/Services/Services';
 import Banner from '../../components/Banner/Banner';
 
 const page = () => {
+  useEffect(() => {
+    // Example: Track a page view with Firebase Analytics
+    analytics.logEvent('page_view', { page_path: '/' });
+  }, []);
   return (
     <div>
       <Banner></Banner>
